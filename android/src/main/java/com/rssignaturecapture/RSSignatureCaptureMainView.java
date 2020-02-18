fc664d641ade8edbc712711b5ad8b61a12455540
friedolin.foerder <friedolin@steuerbot.com>
2020-02-18 10:21:37 +0100

    crop signature and added meta information

--- android/src/main/java/com/rssignaturecapture/RSSignatureCaptureMainView.java
+++ android/src/main/java/com/rssignaturecapture/RSSignatureCaptureMainView.java
@@ -29,6 +29,61 @@ import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import java.lang.Boolean;
 
+class Output {
+  Bitmap bitmap;
+  int xMin;
+  int xMax;
+  int yMin;
+  int yMax;
+  int width;
+  int height;
+  double fillRateAbsolute;
+  double fillRateRelative;
+  double fillAreaRate;
+  public Output(Bitmap bitmap, int xMin, int xMax, int yMin, int yMax, int width, int height, double fillRateAbsolute, double fillRateRelative, double fillAreaRate) {
+    this.bitmap = bitmap;
+    this.xMin = xMin;
+    this.xMax = xMax;
+    this.yMin = yMin;
+    this.yMax = yMax;
+    this.width = width;
+    this.height = height;
+    this.fillRateAbsolute = fillRateAbsolute;
+    this.fillRateRelative = fillRateRelative;
+    this.fillAreaRate = fillAreaRate;
+  }
+  public Bitmap getBitmap() {
+    return bitmap;
+  }
+  public int getXMin() {
+    return xMin;
+  }
+  public int getXMax() {
+    return xMax;
+  }
+  public int getYMin() {
+    return yMin;
+  }
+  public int getYMax() {
+    return yMax;
+  }
+  public int getWidth() {
+    return width;
+  }
+  public int getHeight() {
+    return height;
+  }
+  public double getFillRateAbsolute() {
+    return fillRateAbsolute;
+  }
+  public double getFillRateRelative() {
+    return fillRateRelative;
+  }
+  public double getFillAreaRate() {
+    return fillAreaRate;
+  }
+}
+
 public class RSSignatureCaptureMainView extends LinearLayout implements OnClickListener,RSSignatureCaptureView.SignatureCallback {
   LinearLayout buttonsLayout;
   RSSignatureCaptureView signatureView;
@@ -56,7 +111,7 @@ public class RSSignatureCaptureMainView extends LinearLayout implements OnClickL
     this.addView(signatureView);
 
     setLayoutParams(new android.view.ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-        ViewGroup.LayoutParams.MATCH_PARENT));
+            ViewGroup.LayoutParams.MATCH_PARENT));
   }
 
   public RSSignatureCaptureView getSignatureView() {
@@ -159,28 +214,33 @@ public class RSSignatureCaptureMainView extends LinearLayout implements OnClickL
     }
 
     try {
-
-      Log.d("React Signature", "Save file-======:" + saveFileInExtStorage);
-      // save the signature
-      if (saveFileInExtStorage) {
-        FileOutputStream out = new FileOutputStream(file);
-        this.signatureView.getSignature().compress(Bitmap.CompressFormat.PNG, 90, out);
-        out.flush();
-        out.close();
+      ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+      Bitmap signature = this.signatureView.getSignature();
+      Output output = cropBitmapTransparency(signature);
+      Bitmap bitmap = output.getBitmap();
+      String encoded = null;
+      if(bitmap != null) {
+        bitmap = getResizedBitmap(bitmap);
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream.toByteArray();
+        encoded = "data:image/jpeg;base64," + Base64.encodeToString(byteArray, Base64.DEFAULT);
       }
 
-
-      ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-      Bitmap resizedBitmap = getResizedBitmap(this.signatureView.getSignature());
-      resizedBitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
-
-
-      byte[] byteArray = byteArrayOutputStream.toByteArray();
-      String encoded = Base64.encodeToString(byteArray, Base64.DEFAULT);
-
       WritableMap event = Arguments.createMap();
-      event.putString("pathName", file.getAbsolutePath());
-      event.putString("encoded", encoded);
+      if(encoded == null) {
+        event.putNull("data");
+      } else {
+        event.putString("data", encoded);
+      }
+      event.putInt("xMin", output.getXMin());
+      event.putInt("xMax", output.getXMax());
+      event.putInt("yMin", output.getYMin());
+      event.putInt("yMax", output.getYMax());
+      event.putInt("width", output.getWidth());
+      event.putInt("height", output.getHeight());
+      event.putDouble("fillRateAbsolute", output.getFillRateAbsolute());
+      event.putDouble("fillRateRelative", output.getFillRateRelative());
+      event.putDouble("fillAreaRate", output.getFillAreaRate());
       ReactContext reactContext = (ReactContext) getContext();
       reactContext.getJSModule(RCTEventEmitter.class).receiveEvent(getId(), "topChange", event);
     } catch (Exception e) {
@@ -188,10 +248,82 @@ public class RSSignatureCaptureMainView extends LinearLayout implements OnClickL
     }
   }
 
+
+  public Output cropBitmapTransparency(Bitmap sourceBitmap)
+  {
+    int minX = sourceBitmap.getWidth();
+    int minY = sourceBitmap.getHeight();
+    int maxX = -1;
+    int maxY = -1;
+
+    int imgWidth = sourceBitmap.getWidth();
+    int imgHeight = sourceBitmap.getHeight();
+
+    int filledPixel = 0;
+    int filledWidth = 0;
+    int filledHeight = 0;
+
+    for(int y = 0; y < imgHeight; y++)
+    {
+      for(int x = 0; x < imgWidth; x++)
+      {
+        int alpha = (sourceBitmap.getPixel(x, y) >> 24) & 255;
+        if(alpha > 0)   // pixel is not 100% transparent
+        {
+          filledPixel++;
+          if(x < minX) {
+            minX = x;
+          }
+          if(x > maxX) {
+            maxX = x;
+          }
+          if(y < minY) {
+            minY = y;
+          }
+          if(y > maxY) {
+            maxY = y;
+          }
+        }
+      }
+    }
+    if(filledPixel > 0) {
+      filledWidth = maxX-minX+1;
+      filledHeight = maxY-minY+1;
+    }
+
+    Bitmap bitmap;
+
+    if((maxX < minX) || (maxY < minY)) {
+      bitmap = null;
+    } else {
+      bitmap = Bitmap.createBitmap(sourceBitmap, minX, minY, (maxX - minX) + 1, (maxY - minY) + 1);
+    }
+
+    Output output = new Output(
+            bitmap,
+            minX,
+            maxX,
+            minY,
+            maxY,
+            filledWidth,
+            filledHeight,
+            (double) filledPixel / (imgWidth*imgHeight),
+            filledPixel > 0 ? (double) filledPixel / (filledWidth*filledHeight) : 0,
+            (double) (filledWidth*filledHeight) / (imgWidth*imgHeight)
+    );
+
+    return output;
+  }
+
+
   public Bitmap getResizedBitmap(Bitmap image) {
     Log.d("React Signature","maxSize:"+maxSize);
     int width = image.getWidth();
     int height = image.getHeight();
+
+    if(width <= maxSize) {
+      return image;
+    }
 
     float bitmapRatio = (float) width / (float) height;
     if (bitmapRatio > 1) {
